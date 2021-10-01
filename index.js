80bb1e03a8502be13a47fd45fe6201df29826c80
Abdelkarim EL AMEL <abdelkarim.el.amel@octo.ma>
2021-10-01 14:03:11 +0100

    Setup express

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const cors = require("cors");
+const { get } = require("lodash");
+const origins = get(process, "env.allowedOrigins", "").split(",");
+
+
+// create the server
+const app = express();
+
+// add & configure middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", ...origins],
+    credentials: true,
+  })
+);
+
+
+app.get("/", (req, res) => {
+    return res
+      .status(200)
+      .send({ status: "OK", message: "Hello world" });
+});
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("Listening on " + port);
+});
