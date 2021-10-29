5ab3c7d58bd4f288ec1f69d4692cfb068a4478b8
elos <oussama.elkhalifi@gmail.com>
2021-10-29 18:45:04 +0100

    feat: clean bunnies folder and apply es6 and prettier

--- bunnies/index.js
+++ bunnies/index.js
@@ -1,26 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
-const express = require('express')
-const axios = require('axios')
-const router = express.Router()
-
-router.get('/random',
-async function (req, res) {
+router.get('/random', async (_, res) => {
   try {
-      const {data:{media:{poster}}} = await axios.get("https://api.bunnies.io/v2/loop/random/?media=png,gif");
-      const response = await axios.get(poster, {
-          responseType:"arraybuffer"
-      });
-      const buffer = Buffer.from(response.data, "utf-8")
-      res.end(buffer, 'binary');
+    const {
+      data: {
+        media: { poster },
+      },
+    } = await axios.get('https://api.bunnies.io/v2/loop/random/?media=png,gif');
+    const response = await axios.get(poster, {
+      responseType: 'arraybuffer',
+    });
+    const buffer = Buffer.from(response.data, 'utf-8');
+    res.end(buffer, 'binary');
   } catch (error) {
-
-      console.log(error);
-      return res.status(500).send({
-          message: "An error has occurred while generating your bunnies image !"
-      })
+    return res.status(500).send({
+      message: 'An error has occurred while generating your bunnies image !',
+    });
   }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
