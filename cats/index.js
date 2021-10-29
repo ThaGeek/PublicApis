6e28857936f6619caab8c8bea7109d3c3d410d74
elos <oussama.elkhalifi@gmail.com>
2021-10-29 18:11:10 +0100

    feat :  format cats folder

--- cats/index.js
+++ cats/index.js
@@ -1,30 +1,27 @@
-
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
 const lodash = require('lodash');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/random',
-async function (req, res) {
+router.get('/random', async (req, res) => {
   try {
-      const {data} = await axios.get("https://api.thecatapi.com/v1/images/search");
-      const url = lodash.get(data,"[0].url");
+    const { data } = await axios.get(
+      'https://api.thecatapi.com/v1/images/search',
+    );
+    const url = lodash.get(data, '[0].url');
 
-      const response = await axios.get(url, {
-          responseType:"arraybuffer"
-      });
-      const buffer = Buffer.from(response.data, "utf-8")
-      res.end(buffer, 'binary');
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer',
+    });
+    const buffer = Buffer.from(response.data, 'utf-8');
+    res.end(buffer, 'binary');
   } catch (error) {
-
-      console.log(error);
-      return res.status(500).send({
-          message: "An error has occured while generating your dog image !"
-      })
+    console.log(error);
+    return res.status(500).send({
+      message: 'An error has occured while generating your dog image !',
+    });
   }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
