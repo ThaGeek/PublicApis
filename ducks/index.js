150fb98dd2fbed1b373c481a8dae6b2aaf8ddb2c
elos <oussama.elkhalifi@gmail.com>
2021-10-29 18:18:32 +0100

    feat: cleaning ducks folder, and apply prettier

--- ducks/index.js
+++ ducks/index.js
@@ -1,26 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
-const express = require('express')
-const axios = require('axios')
-const router = express.Router()
-
-router.get('/random',
-async function (req, res) {
+router.get('/random', async (_, res) => {
   try {
-      const {data:{url}} = await axios.get("https://random-d.uk/api/v2/random");
-      const response = await axios.get(url, {
-          responseType:"arraybuffer"
-      });
-      const buffer = Buffer.from(response.data, "utf-8")
-      res.end(buffer, 'binary');
+    const {
+      data: { url },
+    } = await axios.get('https://random-d.uk/api/v2/random');
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer',
+    });
+    const buffer = Buffer.from(response.data, 'utf-8');
+    res.end(buffer, 'binary');
   } catch (error) {
-
-      console.log(error);
-      return res.status(500).send({
-          message: "An error has occured while generating your dog image !"
-      })
+    console.log(error);
+    return res.status(500).send({
+      message: 'An error has occured while generating your dog image !',
+    });
   }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
